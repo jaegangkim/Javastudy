db358392effd13b906bb44ed44a3a5dca925665d
jaegangkim <106479529+jaegangkim@users.noreply.github.com>
2022-06-29 12:36:53 +0900

    이중for문
    이중for문 실습

---
+++ java0628/src/java0628/ex08_세로구구단.java
@@ -0,0 +1,16 @@
+package java0628;
+
+public class ex08_세로구구단 {
+	public static void main(String[] args) {
+		
+		for(int j = 1; j<=9; j++) {
+			
+			for(int i=2;i<=9;i++) {
+				System.out.print(i+"*"+j+"="+j*i+"\t");
+			}
+			System.out.println("");
+		}
+			
+		
+	}
+}
---
+++ java0629/src/java0629/ex01_기본별찍기.java
@@ -0,0 +1,25 @@
+package java0629;
+
+public class ex01_기본별찍기 {
+
+	public static void main(String[] args) {
+		
+		for(int i =1; i<=5 ; i++) {
+			for(int j =1; j<=i ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
